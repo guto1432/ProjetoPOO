6bcf88940167bd37cceeccc072de76479aa47761
guto1432 <f.augusto1432@gmail.com>
2016-05-14 14:30:22 -0300

    campos e construtor

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,4 +1,14 @@
 
 public class Pessoa {
-
+	String Nome;
+	int Idade;
+	double Peso;
+	double Altura;
+	
+	public Pessoa(String sNome, int iIdade, double dPeso, double dAltura){
+		Nome = sNome;
+		Idade = iIdade;
+		Peso = dPeso;
+		Altura = dAltura;
+	}
 }
